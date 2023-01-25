d41a548e7ecd91c40ff2a25cd297df6540f3e58e
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-01-25 13:12:38 +0500

    Update index.js and App.js files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import TodoContainer from './components/TodoContainer';
+import About from './pages/About';
+import SinglePage from './pages/SinglePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<TodoContainer />} />
+      <Route path="/react-todo-app" element={<TodoContainer />} />
+      <Route path="/about">
+        <Route index element={<About />} />
+        <Route path=":slug" element={<SinglePage />} />
+      </Route>
+    </Routes>
+  </>
+);
 
 export default App;
